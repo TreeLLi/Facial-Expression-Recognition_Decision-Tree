bdf2424e1918bb1211aa9ea0bdb62af54c830892
Robin  Jing <yiming.jing17@imperial.ac.uk>
2018-01-29 16:47:15 +0000

    FIRST DRAFT

--- DataProcessor.py
+++ DataProcessor.py
@@ -3,18 +3,38 @@
 # Load data
         
 def readDataFromMat(file_name):
-    print ("read data: " + file_name)
-    return [[1, 2, 3], [4, 5, 6]]
-
+    print("read data: " + file_name)
+    cleanDataX = scipy.io.loadmat(file_name)['x']
+    cleanDataY = scipy.io.loadmat(file_name)['y']
+    num_of_rows = len(cleanDataY)
+    #print(num_of_rows)
+    newcleandataY = np.zeros((num_of_rows,6),int)
+    for row in range(newcleandataY.shape[0]):
+        col  = cleanDataY[row]-1
+        newcleandataY[row][col] = 1
+    return (cleanDataX, newcleandataY)
 # Split and divide
 
 def crossValidation(dataset, fold, fold_num):
+    set_length=len(dataset) / float(fold_num)
+    n = int(math.ceil(set_length))
+    set_no = (fold-1)*n
+    train_list = copy.deepcopy(dataset[set_no:set_no+n])
+    test_list = copy.deepcopy(dataset)
+    del test_list[set_no:set_no+n]
+
     # return (train_list, test_list)
     # train_list is [[x-samples:N*45], [y-labels:1*N]]
-    return ([[1, 2, 3], [2, 3, 4]], [[5, 6, 7], [8, 9, 10]])
+    return (train_list,test_list)
 
 def subDataset(dataset, attr):
-    # split dataset based on the values of given attr
-    # return sub-datasets with the same form of dataset
-    # idx 0 for attr value 0, idx 1 for attr value 1
-    return [dataset, dataset]
+    dataset_tmp = copy.deepcopy(dataset)
+    dataset_tmp.sort( key=lambda attribute:attribute[attr-1])
+    count = 0
+    for i in dataset_tmp:
+        if i[attr-1] == 1:
+            break
+        count = count+1
+    zero_dataset = dataset_tmp[0:count]
+    first_dataset = dataset_tmp[count:]
+    return [zero_dataset, first_dataset]
